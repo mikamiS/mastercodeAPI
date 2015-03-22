0684bac39da73ce34ad9884b405d834aa7cbe5e7
S <S@Ss-MacBook-Air.local>
2015-03-22 09:10:29 +0800

    remove debug code

--- WalletWebContent/A1.php
+++ WalletWebContent/A1.php
@@ -3,7 +3,7 @@ require_once('Controller/MasterPassController.php');
 require_once('Controller/MasterPassHelper.php');
 
 session_start();
-// header('Content-Type: text/javascript; charset=utf-8');
+header('Content-Type: text/javascript; charset=utf-8');
 //////////////////////////////////////////////////////////
 // from index.html
 //////////////////////////////////////////////////////////
@@ -52,44 +52,3 @@ $result["version"] = "v6";
 
 echo json_encode($result);
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-
-<html>
-<head>
-	<title>Shopping Cart Sample Flow</title>
-	<link rel="stylesheet" type="text/css" href="Content/Site.css" />
-	<script type="text/javascript" src="Scripts/jquery-1.5.1.js"></script>
-    <script type="text/javascript" src="Scripts/common.js"></script>
-    <script type="text/javascript" src="Scripts/tooltips/commonToolTips.js"></script>
-    <script type="text/javascript" src="Scripts/tooltips/jquery-1.3.2.min.js"></script> 
-	<script type="text/javascript" src="Scripts/tooltips/jquery.qtip-1.0.0-rc3.min.js"></script>
-	<script type="text/javascript" src="<?php echo $sad->lightboxUrl ?>"></script>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-</head>
-<body>
-	<script type="text/javascript" language="Javascript">
-	
-		function handleBuyWithMasterPass() {
-			MasterPass.client.checkout({
-       			 "requestToken":"<?php echo $sad->requestToken ?>",
-       			 "callbackUrl":"http://localhost:8080/mastercode/WalletWebContent/A2.php",
-       			 "merchantCheckoutId":"<?php echo $sad->checkoutIdentifier ?>",
-       			 "allowedCardTypes":"<?php echo $sad->acceptableCards ?>",
-       			 "cancelCallback" : "<?php echo $sad->callbackDomain ?>",
-       			 "suppressShippingAddressEnable":false,
-       			 "loyaltyEnabled" :false,
-       			 "requestBasicCheckout" : false,
-       			 // "requestExpressCheckout": true,
-       		 	 "version":"v6"
-       		});
-		}
-		
-	</script>
-	<div id="checkoutButtonDiv" onClick="handleBuyWithMasterPass()">
-		<a href="#">
-			<img src="https://www.mastercard.com/mc_us/wallet/img/en/US/mcpp_wllt_btn_chk_147x034px.png" alt="Buy with MasterPass">
-		</a>
-	</div>
-</body>
-</html>
